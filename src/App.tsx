7c3e2189a9a52064b49b705380d78694bd545254
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2025-02-09 22:51:38 -0600

    React Router Dom has been setup.

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,19 @@
+//Import dependencies
 import './App.css'
+import { Route, Routes } from 'react-router'
 
 //Pages
 import Home from './pages/Home'
+import Error from './pages/Error'
 
 function App() {
 
   return (
     <div className="App">
-
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<Error />} />
+      </Routes>
     </div>
   )
 }
---
+++ src/pages/Error.tsx
@@ -0,0 +1,7 @@
+export default function Error(){
+    return(
+        <div className="Error">
+            <h2>Error. The page you're trying to visit doesn't exist.</h2>
+        </div>
+    )
+}
