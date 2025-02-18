d857d6b99de326388c43937dfd5b8d6c023bf751
Joshua Smith <61332947+joshuatsmith96@users.noreply.github.com>
2025-02-18 17:07:23 -0600

    Items now get added to cart
    When you click the "Add To Cart" button, it now populates the items in the cart.

--- src/pages/Cart.tsx
+++ src/pages/Cart.tsx
@@ -1,7 +1,36 @@
+import { useState } from 'react';
+import Data from '../test-data.json'
+
 const Cart = () => {
+    let cartObjects = localStorage.getItem("cart");
+    cartObjects = cartObjects ? JSON.parse(cartObjects) : []; // Ensure it's an array
+    let cartData = []
+
+    //Get all details for cart objects
+    if(cartObjects != null){
+        for(let i=0; i<=cartObjects.length-1; i++){
+            let id = Array.isArray(cartObjects) && cartObjects.length > 0 ? cartObjects[i].id : "";
+            cartData.push(Data.filter(item => item.id === id)[0]);
+        }
+    }
+
+    console.log(cartData)
+
     return(
-        <div className="Cart">
-            <h1>Cart</h1>
+        <div className="Cart px-4 pb-[400px]">
+            <h1 className="text-2xl font-medium">Shopping Cart</h1>
+            <p>{cartData != null && cartData.length > 0 ? "" : "Cart Is Empty"}</p>
+
+            <div>
+                {cartData.map((item) => {
+                    return(
+                        <div className='border p-5 my-3'>
+                            <h1>{item.name}</h1>
+                            <p>{item.price}</p>
+                        </div>
+                    )
+                })}
+            </div>
         </div>
     )
 }
